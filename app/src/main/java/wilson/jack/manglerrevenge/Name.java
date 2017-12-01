39b2c2b74b78d457a8f32edab6cbb1b41650d80d
JackEWilson <jack.wilson.east@gmail.com>
2017-12-01 13:49:59 -0800

    added nameviewmodel

--- app/src/main/java/wilson/jack/manglerrevenge/Name.java
+++ app/src/main/java/wilson/jack/manglerrevenge/Name.java
@@ -5,21 +5,18 @@ import java.util.List;
 import java.util.Random;
 
 public class Name {
-    private Random mRandomizer;
     private List<String> mNiceNameList = Arrays.asList("Awesome", "TwoShoes", "Wilson", "Super", "Cool");
     private List<String> mRudeNameList = Arrays.asList("Bad", "Evil", "Silly", "Ugly", "Stupid");
 
     public Name(){
-        mRandomizer = new Random();
+
     }
 
-    public String getRudeName() {
-        String random = mNiceNameList.get(mRandomizer.nextInt(mNiceNameList.size()));
-        return random;
+    public List<String> getNiceNameList() {
+        return mNiceNameList;
     }
 
-    public String getNiceName() {
-        String random = mRudeNameList.get(mRandomizer.nextInt(mRudeNameList.size()));
-        return random;
+    public List<String> getRudeNameList() {
+        return mRudeNameList;
     }
 }
---
+++ app/src/main/java/wilson/jack/manglerrevenge/NameViewModel.java
@@ -0,0 +1,27 @@
+package wilson.jack.manglerrevenge;
+
+import java.util.List;
+import java.util.Random;
+
+public class NameViewModel {
+    private Random mRandomizer;
+    private Name mName;
+
+    public NameViewModel() {
+        mRandomizer = new Random();
+        mName = new Name();
+    }
+
+
+    public String getNiceName(){
+        List<String> mNameList = mName.getNiceNameList();
+        String random = mNameList.get(mRandomizer.nextInt(mNameList.size()));
+        return random;
+    }
+
+    public String getRudeName(){
+        List<String> mNameList = mName.getRudeNameList();
+        String random = mNameList.get(mRandomizer.nextInt(mNameList.size()));
+        return random;
+    }
+}
